751e01f79ddeeb8aa07f08938745265fe240719d
Juan Gerardo <jmendez1025@cue.edu.co>
2025-11-03 07:47:45 -0500

    feat: allow filtering academic programs  based on facultyId

--- src/features/academicprogram/hooks/useAllAcademicPrograms.ts
+++ src/features/academicprogram/hooks/useAllAcademicPrograms.ts
@@ -2,10 +2,11 @@ import { useQuery } from "@tanstack/react-query";
 import { ACADEMIC_PROGRAM_QUERY_KEYS } from "../constants/academicProgramQueries.constant";
 import type { AcademicProgram } from "@/domain/academicprogram/AcademicProgram";
 import { getAllAcademicPrograms } from "@/services/academicProgram.service";
+import type { GetAllAcademicProgramRequestDto } from "@/domain/academicprogram/GetAllAcademicProgramsRequestDto";
 
-export function useAllAcademicPrograms() {
+export function useAllAcademicPrograms(query?: GetAllAcademicProgramRequestDto) {
     return useQuery<AcademicProgram[], Error>({
-        queryKey: [ACADEMIC_PROGRAM_QUERY_KEYS.ACADEMIC_PROGRAMS.ALL],
-        queryFn: () => getAllAcademicPrograms(),
+        queryKey: [ACADEMIC_PROGRAM_QUERY_KEYS.ACADEMIC_PROGRAMS.ALL, query],
+        queryFn: () => getAllAcademicPrograms(query),
     });
 }
