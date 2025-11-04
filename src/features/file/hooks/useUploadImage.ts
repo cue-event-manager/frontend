fdc6f076fa2086cb5c8e2f73f164cfeedf9922c7
Juan Gerardo <jmendez1025@cue.edu.co>
2025-11-03 22:28:21 -0500

    feat(hooks): enhance file and space hooks with improved error handling and UX feedback

--- src/features/file/hooks/useUploadImage.ts
+++ src/features/file/hooks/useUploadImage.ts
@@ -1,7 +1,7 @@
 import { useMutation } from "@tanstack/react-query";
 import type { File } from "@/domain/file/File";
-import type { UploadImageRequestDto } from "@/domain/file/UploadImageRequestDto";
 import { uploadImage } from "@/services/file.service"; 
+import type { UploadImageRequestDto } from "@/domain/file/UploadImageRequestDto";
 
 export function useUploadImage() {
   return useMutation<File, Error, UploadImageRequestDto>({
--- src/services/file.service.ts
+++ src/services/file.service.ts
@@ -1,6 +1,7 @@
 import type { File } from "@/domain/file/File";
 import { FILE_SERVICE } from "./constants";
 import type { UploadImageRequestDto } from "@/domain/file/UploadImageRequestDto";
+import type { UploadFileRequestDto } from "@/domain/file/UploadFileRequestDto";
 import axiosInstance from "@/config/axiosConfig";
 
 const FILE_ENDPOINT_PREFIX = `${FILE_SERVICE}/api/files`;
@@ -24,3 +25,24 @@ export const uploadImage = async (
 
     return data;
 };
+
+export const uploadFile = async (
+    uploadFileRequest: UploadFileRequestDto
+): Promise<File> => {
+    const formData = new FormData();
+    formData.append("file", uploadFileRequest.file);
+
+    const { data } = await axiosInstance.post<File>(
+        `${FILE_ENDPOINT_PREFIX}/upload-file`,
+        formData,
+        {
+            transformRequest: (data) => data,
+            headers: {
+                "Content-Type": undefined,
+            },
+        }
+    );
+
+    return data;
+};
+
