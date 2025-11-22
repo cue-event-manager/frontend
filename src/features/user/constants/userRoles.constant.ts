e88a415e739b5b298e9ca2161e8530e93570c987
Juan Gerardo <jmendez1025@cue.edu.co>
2025-11-22 07:54:44 -0500

    fix: role column in  users table

---
+++ src/features/user/constants/userRoles.constant.ts
@@ -0,0 +1,5 @@
+export const ROLES = [
+    { value: "1", label: "Administrador", name: "ADMIN", },
+    { value: "2", label: "Organizador", name: "ORGANIZER" },
+    { value: "3", label: "Asistente", name: "ATTENDEE" },
+];
